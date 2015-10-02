dfede8f2f2f18e7e3a823d0e5bd7289f0926473f
Jordan Neufeld <jordan@neufeldtech.com>
2015-10-01 22:19:24 -0500

    working scraping basic restaurant name

--- server.js
+++ server.js
@@ -6,40 +6,44 @@ var app     = express();
 
 app.get('/scrape', function(req, res){
 	// Let's scrape Anchorman 2
-	url = 'http://www.imdb.com/title/tt1229340/';
+	url = 'https://www.skipthedishes.com/winnipeg/restaurants';
 
 	request(url, function(error, response, html){
 		if(!error){
 			var $ = cheerio.load(html);
 
 			var title, release, rating;
-			var json = { title : "", release : "", rating : ""};
+			var jsonarray = [];
 
-			$('.header').filter(function(){
+			$('.restaurant-name').filter(function(){
 		        var data = $(this);
-		        title = data.children().first().text();
-		        release = data.children().last().children().text();
 
-		        json.title = title;
-		        json.release = release;
-	        })
-
-	        $('.star-box-giga-star').filter(function(){
-	        	var data = $(this);
-	        	rating = data.text();
-
-	        	json.rating = rating;
-	        })
+		    //    title = data.children().first().text();
+//
+	//	        json.title = title;
+		//        json.release = release;
+	    		//console.log(data);
+					for (d=0; d < data.length; d++){
+						//console.log('----------------------------');
+						var restaurant = data[d].children[0].data
+						//console.log(restaurant);
+						var json = { name : "" };
+						json.name = restaurant ;
+						jsonarray.push(json);
+					}
+			    })
+					console.log(jsonarray);
+					console.log(jsonarray.length);
 		}
-
+/*
 		fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
         	console.log('File successfully written! - Check your project directory for the output.json file');
         })
-
-        res.send('Check your console!')
+*/
+        res.json(jsonarray);
 	})
 })
 
 app.listen('8081')
 console.log('Magic happens on port 8081');
-exports = module.exports = app; 	
+exports = module.exports = app;
