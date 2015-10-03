6875ec92f968cc04c728e7373cb895f525921312
Jordan Neufeld <jordan@neufeldtech.com>
2015-10-03 17:17:55 -0500

    scraping working with JSON returned via server.  Still need to add caching

--- server.js
+++ server.js
@@ -2,38 +2,40 @@ var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
-var app     = express();
+var app = express();
+//adding these to decode html elements
+var Encoder = require('node-html-encoder').Encoder;
+var encoder = new Encoder('entity');
 
 app.get('/scrape', function(req, res){
-	// Let's scrape Anchorman 2
 	url = 'https://www.skipthedishes.com/winnipeg/restaurants';
 	request(url, function(error, response, html){
 		if(!error){
 			var $ = cheerio.load(html);
 			var jsonarray = [];
 
-
+			//$('.restaurant-list').each(function(){ console.log($(this).attr('data-restaurant-name'));});
+			var baseUrl = "https://www.skipthedishes.com"
 			$('.restaurant-list').each(function(){
-				console.log($(this).attr('data-restaurant-name'));
+				var restaurantName = $(this).find('span.restaurant-name').html();
+				var link = $(this).find('a.show-loading').attr('href');
+				var rating = $(this).attr('data-restaurant-rating');
+				restaurantNamePretty = encoder.htmlDecode(restaurantName.replace(/\&apos;/g, "'"));
+				//build the JSON object then add it to the array
+				var json = {name : '', url : '', rating : ''};
+				json.name = restaurantNamePretty;
+				json.url = baseUrl+link;
+				json.rating = rating;
+				jsonarray.push(json);
 			});
-			/*
-			for (r=0;r < restaurants.length;r++) {
-	      var restaurant = restaurants[r];
-				restaurant = $(restaurant);
-				console.log(restaurant);
-				//var json = { name : "" };
-				//json.name = restaurant ;
-				//jsonarray.push(json);
-			}
-			*/
 		}//end if error
-		//fakeresponse
-		jsonarray.push({'status':'OK'});
-		//endfakeresponse
-  	res.json(jsonarray);
+		//build final JSON object
+		var restaurantsJson = { restaurants : '' };
+		restaurantsJson.restaurants = jsonarray;
+  	res.json(restaurantsJson);
 	})//end request
 })//end app.get call
 
-app.listen('8081')
+app.listen('8081');
 console.log('Magic happens on port 8081');
 exports = module.exports = app;
