fa6c7889904bdb6d8136d29962bc498b3d9f6b3e
Jordan Neufeld <jordan@neufeldtech.com>
2015-10-03 16:15:03 -0500

    iteration working 1

--- server.js
+++ server.js
@@ -7,42 +7,32 @@ var app     = express();
 app.get('/scrape', function(req, res){
 	// Let's scrape Anchorman 2
 	url = 'https://www.skipthedishes.com/winnipeg/restaurants';
-
 	request(url, function(error, response, html){
 		if(!error){
 			var $ = cheerio.load(html);
-
-			var title, release, rating;
 			var jsonarray = [];
 
-			$('.restaurant-name').filter(function(){
-		        var data = $(this);
 
-		    //    title = data.children().first().text();
-//
-	//	        json.title = title;
-		//        json.release = release;
-	    		//console.log(data);
-					for (d=0; d < data.length; d++){
-						//console.log('----------------------------');
-						var restaurant = data[d].children[0].data
-						//console.log(restaurant);
-						var json = { name : "" };
-						json.name = restaurant ;
-						jsonarray.push(json);
-					}
-			    })
-					console.log(jsonarray);
-					console.log(jsonarray.length);
-		}
-/*
-		fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-        	console.log('File successfully written! - Check your project directory for the output.json file');
-        })
-*/
-        res.json(jsonarray);
-	})
-})
+			$('.restaurant-list').each(function(){
+				console.log($(this).attr('data-restaurant-name'));
+			});
+			/*
+			for (r=0;r < restaurants.length;r++) {
+	      var restaurant = restaurants[r];
+				restaurant = $(restaurant);
+				console.log(restaurant);
+				//var json = { name : "" };
+				//json.name = restaurant ;
+				//jsonarray.push(json);
+			}
+			*/
+		}//end if error
+		//fakeresponse
+		jsonarray.push({'status':'OK'});
+		//endfakeresponse
+  	res.json(jsonarray);
+	})//end request
+})//end app.get call
 
 app.listen('8081')
 console.log('Magic happens on port 8081');
