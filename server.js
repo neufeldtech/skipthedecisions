960c06e251b0c529727849e9b2cea2bf6ebf0b61
Jordan Neufeld <myjc.niv@gmail.com>
2015-10-12 16:59:56 -0500

    turning off CORS, turning on 1 day caching headers for client static content

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/skipApp');
 var Collection = require('./models/collection');
 var apicache = require('apicache').options({ debug: true }).middleware;
-//configurations
+
+//uncomment this to turn on CORS
+/*
 var cors = require('cors')
 var corsOptions = {
   origin: '*'
 };
 app.use(cors(corsOptions));
+*/
 
 app.get('/restaurants', apicache('30 seconds'), function(req, res){
   Collection.find(function(err, restaurants) {
@@ -27,7 +30,10 @@ app.get('/restaurants', apicache('30 seconds'), function(req, res){
 
 //serve static files in public directory
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
+//serve static content from /public.  Set cache headers to one day
+var oneDay = 86400000;
+app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
 
 app.get('/cities', apicache('30 seconds'), function(req, res){
   Collection.distinct('city', function(err, restaurants) {
